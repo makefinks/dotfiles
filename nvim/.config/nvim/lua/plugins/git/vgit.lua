8879b0db728902ffd375d3aefa4e31446e9b6f0e
Leon Fink <Leon.fink.lf@gmail.com>
2025-12-03 23:16:53 +0100

    add new vgit global, buffer and branch unified diffs, remove sidekick, add new theme

--- nvim/.config/nvim/lua/plugins/git/vgit.lua
+++ nvim/.config/nvim/lua/plugins/git/vgit.lua
@@ -1,7 +1,7 @@
 return {
 	"tanvirtin/vgit.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
-	event = "VimEnter",
+	lazy = true,
 	keys = {
 		-- Diff views
 		{
@@ -17,6 +17,16 @@ return {
 				require("vgit").project_diff_preview()
 			end,
 			desc = "Project diff",
+		},
+		{
+			"<leader>gb",
+			function()
+				local branch = vim.fn.input("Branch: ", "main")
+				if branch ~= "" then
+					require("vgit").buffer_diff_preview({ branch = branch })
+				end
+			end,
+			desc = "Buffer diff against branch",
 		},
 
 		-- Conflict resolution
@@ -49,6 +59,32 @@ return {
 				live_gutter = { enabled = false },
 				scene = {
 					diff_preference = "unified",
+				},
+				hls = {
+					GitSignsAdd = {
+						fg = "#5a9dc7",
+						bg = nil,
+					},
+					GitSignsDelete = {
+						fg = "#cc0000",
+						bg = nil,
+					},
+					GitSignsAddLn = {
+						fg = "#5a9dc7",
+						bg = "#2a3f4d",
+					},
+					GitSignsDeleteLn = {
+						fg = "#cc0000",
+						bg = "#4d2a2a",
+					},
+					GitWordAdd = {
+						fg = nil,
+						bg = "#2a3f4d",
+					},
+					GitWordDelete = {
+						fg = nil,
+						bg = "#4d2a2a",
+					},
 				},
 			},
 		})
