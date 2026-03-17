105577d76ac819fec27626a91b7fa4e8b4a92f91
makefinks <leon.fink.lf@gmail.com>
2026-03-17 23:11:40 +0100

    adjust keybinds

--- nvim/.config/nvim/lua/plugins/git/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/git/gitsigns.lua
@@ -1,13 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = function(_, opts)
-		-- Remove <leader>gd from gitsigns (we use codediff instead)
+		-- Remove gitsigns hunk-stage mappings so codediff can own <leader>gs for whole-buffer staging.
 		if opts.on_attach then
 			local original_on_attach = opts.on_attach
 			opts.on_attach = function(bufnr)
 				original_on_attach(bufnr)
 				-- gitsigns key defaults can change across versions; avoid hard failures.
 				pcall(vim.keymap.del, "n", "<Leader>gd", { buffer = bufnr })
+				pcall(vim.keymap.del, "n", "<Leader>gs", { buffer = bufnr })
+				pcall(vim.keymap.del, "v", "<Leader>gs", { buffer = bufnr })
 			end
 		end
 	end,
