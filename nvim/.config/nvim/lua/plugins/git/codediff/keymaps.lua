89e6f462b8e7edea0b5ff8b90b517cc69347b657
makefinks <leon.fink.lf@gmail.com>
2026-03-19 21:57:23 +0100

    Refine codediff buffer keymaps

--- nvim/.config/nvim/lua/plugins/git/codediff/keymaps.lua
+++ nvim/.config/nvim/lua/plugins/git/codediff/keymaps.lua
@@ -9,6 +9,23 @@ function M.set_tab_keymaps(tabpage, get_codediff_lifecycle, deps)
     return
   end
 
+  local function set_buffer_keymap(bufnr, lhs, rhs, desc)
+    if not (bufnr and vim.api.nvim_buf_is_valid(bufnr)) then
+      return
+    end
+
+    session.keymap_buffers = session.keymap_buffers or {}
+    session.keymap_buffers[bufnr] = true
+
+    vim.keymap.set("n", lhs, rhs, {
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      nowait = true,
+      desc = desc,
+    })
+  end
+
   lifecycle.set_tab_keymap(tabpage, "n", "ff", function()
     deps.actions.open_file_picker(get_codediff_lifecycle, tabpage)
   end, { desc = "Search files in codediff" })
@@ -17,40 +34,23 @@ function M.set_tab_keymaps(tabpage, get_codediff_lifecycle, deps)
     deps.view.toggle_explorer(get_codediff_lifecycle, tabpage)
   end, { desc = "Toggle codediff explorer" })
 
-  lifecycle.set_tab_keymap(tabpage, "n", "<leader>gs", function()
-    deps.actions.toggle_stage(get_codediff_lifecycle, tabpage)
-  end, { desc = "Stage/unstage current entry" })
-
-  lifecycle.set_tab_keymap(tabpage, "n", "<leader>gu", function()
-    deps.actions.unstage_entry(get_codediff_lifecycle, tabpage)
-  end, { desc = "Unstage current entry" })
-
-  lifecycle.set_tab_keymap(tabpage, "n", "<leader>gx", function()
-    deps.actions.restore_entry(get_codediff_lifecycle, tabpage)
-  end, { desc = "Discard current entry" })
-
   lifecycle.set_tab_keymap(tabpage, "n", "<C-q>", function()
     deps.view.close_view(get_codediff_lifecycle)
   end, { desc = "Close codediff view" })
 
   local original_bufnr, modified_bufnr = lifecycle.get_buffers(tabpage)
   for _, bufnr in ipairs({ original_bufnr, modified_bufnr }) do
-    if bufnr and vim.api.nvim_buf_is_valid(bufnr) then
-      session.keymap_buffers = session.keymap_buffers or {}
-      session.keymap_buffers[bufnr] = true
+    set_buffer_keymap(bufnr, "<leader>gs", function()
+      deps.actions.stage_entry(get_codediff_lifecycle, tabpage)
+    end, "Stage current entry")
 
-      vim.keymap.set("n", "<leader>gs", function()
-        deps.actions.stage_entry(get_codediff_lifecycle, tabpage)
-      end, { buffer = bufnr, noremap = true, silent = true, nowait = true, desc = "Stage current entry" })
+    set_buffer_keymap(bufnr, "<leader>gu", function()
+      deps.actions.unstage_entry(get_codediff_lifecycle, tabpage)
+    end, "Unstage current entry")
 
-      vim.keymap.set("n", "<leader>gu", function()
-        deps.actions.unstage_entry(get_codediff_lifecycle, tabpage)
-      end, { buffer = bufnr, noremap = true, silent = true, nowait = true, desc = "Unstage current entry" })
-
-      vim.keymap.set("n", "<leader>gx", function()
-        deps.actions.restore_entry(get_codediff_lifecycle, tabpage)
-      end, { buffer = bufnr, noremap = true, silent = true, nowait = true, desc = "Discard current entry" })
-    end
+    set_buffer_keymap(bufnr, "<leader>gx", function()
+      deps.actions.restore_entry(get_codediff_lifecycle, tabpage)
+    end, "Discard current entry")
   end
 
   local explorer = lifecycle.get_explorer(tabpage)
@@ -59,34 +59,44 @@ function M.set_tab_keymaps(tabpage, get_codediff_lifecycle, deps)
   end
   if explorer and explorer.bufnr and vim.api.nvim_buf_is_valid(explorer.bufnr) then
     local ok_navigation, navigation = pcall(require, "codediff.ui.view.navigation")
-    session.keymap_buffers = session.keymap_buffers or {}
-    session.keymap_buffers[explorer.bufnr] = true
 
-    vim.keymap.set("n", "<CR>", function()
+    set_buffer_keymap(explorer.bufnr, "<CR>", function()
       deps.view.open_explorer_entry(get_codediff_lifecycle, tabpage, explorer)
-    end, { buffer = explorer.bufnr, noremap = true, silent = true, nowait = true, desc = "Open current codediff entry" })
+    end, "Open current codediff entry")
 
     if ok_navigation then
-      vim.keymap.set("n", "<Tab>", function()
+      set_buffer_keymap(explorer.bufnr, "<Tab>", function()
         navigation.next_file()
-      end, { buffer = explorer.bufnr, noremap = true, silent = true, nowait = true, desc = "Next codediff file" })
+      end, "Next codediff file")
 
-      vim.keymap.set("n", "<S-Tab>", function()
+      set_buffer_keymap(explorer.bufnr, "<S-Tab>", function()
         navigation.prev_file()
-      end, { buffer = explorer.bufnr, noremap = true, silent = true, nowait = true, desc = "Previous codediff file" })
+      end, "Previous codediff file")
     end
 
-    vim.keymap.set("n", "s", function()
-      deps.actions.stage_entry(get_codediff_lifecycle, tabpage)
-    end, { buffer = explorer.bufnr, noremap = true, silent = true, nowait = true, desc = "Stage current entry" })
+    set_buffer_keymap(explorer.bufnr, "<leader>gs", function()
+      deps.actions.toggle_stage(get_codediff_lifecycle, tabpage)
+    end, "Stage/unstage current entry")
 
-    vim.keymap.set("n", "u", function()
+    set_buffer_keymap(explorer.bufnr, "<leader>gu", function()
       deps.actions.unstage_entry(get_codediff_lifecycle, tabpage)
-    end, { buffer = explorer.bufnr, noremap = true, silent = true, nowait = true, desc = "Unstage current entry" })
+    end, "Unstage current entry")
 
-    vim.keymap.set("n", "x", function()
+    set_buffer_keymap(explorer.bufnr, "<leader>gx", function()
       deps.actions.restore_entry(get_codediff_lifecycle, tabpage)
-    end, { buffer = explorer.bufnr, noremap = true, silent = true, nowait = true, desc = "Discard current entry" })
+    end, "Discard current entry")
+
+    set_buffer_keymap(explorer.bufnr, "s", function()
+      deps.actions.stage_entry(get_codediff_lifecycle, tabpage)
+    end, "Stage current entry")
+
+    set_buffer_keymap(explorer.bufnr, "u", function()
+      deps.actions.unstage_entry(get_codediff_lifecycle, tabpage)
+    end, "Unstage current entry")
+
+    set_buffer_keymap(explorer.bufnr, "x", function()
+      deps.actions.restore_entry(get_codediff_lifecycle, tabpage)
+    end, "Discard current entry")
   end
 end
 
